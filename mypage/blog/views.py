3dee26b56543e100d8e2d425a40f3652abd2faeb
Vo Quoc Quang <vqquangys25@gmail.com>
2024-08-14 19:32:11 +0700

    update sharing post feature

--- mypage/blog/views.py
+++ mypage/blog/views.py
@@ -3,8 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
-
-
+from django.core.mail import send_mail
 
 def post_share(request, post_id):
     # Retrieve post by id
@@ -13,7 +12,8 @@ def post_share(request, post_id):
         id = post_id,
         status = Post.Status.PUBLISHED
     )
-    
+    sent = False
+        
     if request.method == 'POST':
         # Form was submitte
         form = EmailPostForm(request.POST)
@@ -21,6 +21,24 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
     else:
         form = EmailPostForm()
     return render(
@@ -28,7 +46,8 @@ def post_share(request, post_id):
         'blog/post/share.html',
         {
             'post' : post,
-            'form' : form
+            'form' : form,
+            'sent' : sent
         }
     )
 
