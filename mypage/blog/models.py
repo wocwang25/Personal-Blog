3290a104bf14bc3d3e18b317e5c0bdc3458e50a1
Vo Quoc Quang <vqquangys25@gmail.com>
2024-08-15 15:33:40 +0700

    add comment feature

--- mypage/blog/models.py
+++ mypage/blog/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
     
     objects = models.Manager()
     published = PublishedManager()
-    
+
     class Meta():
         ordering = ['-publish']
         indexes = [
@@ -62,3 +62,25 @@ class Post(models.Model):
         )
     
 print(Post.Status.choices)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+    def __str__(self) -> str:
+        return f'Comment by {self.name} on {self.post}'
