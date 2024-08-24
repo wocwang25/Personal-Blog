242e36d4df2ac3ab7e29a52c6de4949650aecf99
Vo Quoc Quang <vqquangys25@gmail.com>
2024-08-25 00:14:29 +0700

    changing database from SQLite into PostgreSQL

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -52,10 +52,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'django.contrib.postgres',
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    
 ]
 
 MIDDLEWARE = [
@@ -94,11 +96,13 @@ WSGI_APPLICATION = 'mypage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
